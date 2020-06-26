0dc369d8e58d83e95e8c19fadac845c20f880c3c
Swathi B R <swathi.br99@gmail.com>
2020-06-26 11:30:13 +0530

    Commit1

--- Term_Insurances_AppLication/Controllers/HomeController.cs
+++ Term_Insurances_AppLication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Term_Insurances_AppLication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+           
 
             return View();
         }
